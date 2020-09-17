c15c410fa2e24eb35c4809f47475aa8259ad0a4b
Łukasz Macoń <luk.macon@gmail.com>
2020-09-17 21:42:39 +0200

    set theme proviedr for styled components

---
+++ constants/theme.tsx
@@ -0,0 +1,10 @@
+export const theme = {
+  light: {
+    main: "#de393f",
+    hoverMain: "#9c2126",
+    accent: "#f49000",
+    hoverAccent: "#c97700",
+    textPrimary: "#ffffff",
+    textSecondary: "#d0d0d0",
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,22 @@ import "../styles/globals.scss";
 import App from "next/app";
 import { appWithTranslation } from "../i18n";
 
+import { ThemeProvider } from "styled-components";
+import { theme } from "../constants/Theme";
+
 const makestore = () => store;
 const wrapper = createWrapper(makestore);
 
 const MyApp = ({ Component, pageProps }) => (
   <Provider store={store}>
-    <Component {...pageProps} />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   </Provider>
 );
 
-MyApp.getInitialProps = async appContext => ({
-  ...(await App.getInitialProps(appContext))
+MyApp.getInitialProps = async (appContext) => ({
+  ...(await App.getInitialProps(appContext)),
 });
 
 export default wrapper.withRedux(appWithTranslation(MyApp));
